794ec003165fdfbfc1d454008716ca6159bf77e5
zhangrj <zhang_career@foxmail.com>
2026-01-05 16:37:03 +0800

    HttpRequestContext lower-case constants added

--- src/Request/HttpRequestContext.php
+++ src/Request/HttpRequestContext.php
@@ -33,10 +33,13 @@ use PSX\Http\RequestInterface;
 class HttpRequestContext implements RequestContextInterface
 {
     public const X_REQUEST_ID = 'X-Request-Id';
+    public const X_REQUEST_ID_LOWER = 'x-request-id';
 
     public const X_API_KEY = 'X-API-Key';
+    public const X_API_KEY_LOWER = 'x-api-key';
 
     public const X_EVENT_CODE = 'X-Event-Code';
+    public const X_EVENT_CODE_LOWER = 'x-event-code';
 
 
     private RequestInterface $request;
