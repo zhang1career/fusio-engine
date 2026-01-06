2c89952a0136c9d9db744fc48717aa13035691f2
zhangrj <zhang_career@foxmail.com>
2026-01-06 11:03:03 +0800

    refactor constants

---
+++ src/Request/HttpRequestHeaderConstant.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Fusio\Engine\Request;
+
+class HttpRequestHeaderConstant
+{
+    public const AUTHORIZATION = 'Authorization';
+    public const AUTHORIZATION_LOWER = 'authorization';
+
+    public const X_REQUEST_ID = 'X-Request-Id';
+    public const X_REQUEST_ID_LOWER = 'x-request-id';
+
+    public const X_API_KEY = 'X-API-Key';
+    public const X_API_KEY_LOWER = 'x-api-key';
+
+    public const X_EVENT_CODE = 'X-Event-Code';
+    public const X_EVENT_CODE_LOWER = 'x-event-code';
+}
